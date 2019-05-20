b5e7f2ea81278df22d8e77e589425afec91f0e67
Nic Fosland <ntfosland@gmail.com>
2019-05-20 10:30:33 -0500

    Added a input question loop

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -4,10 +4,40 @@ namespace Hello_World
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            string input;
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Github!");
+
+
+
+            bool keepGoing;
+            keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.WriteLine("type yes or no");
+
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "yes":
+                        Console.WriteLine("you typed yes");
+                        break;
+                    case "no":
+                        Console.WriteLine("you typed no");
+                        break;
+                    default:
+                        Console.WriteLine("you didnt type either yes or no");
+                        keepGoing = false;
+                        break;
+                }
+            }
+
+
         }
     }
 }
